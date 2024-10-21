591c79282ffbcdeb246ee718cb2180e23e916998
Suara33 <tornike.suarishvili@gmail.com>
2024-10-21 17:11:45 +0400

    feat: add line in files services

--- src/files/services/s3.service.ts
+++ src/files/services/s3.service.ts
@@ -1,19 +1,19 @@
-import { Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
-import { ManagedUpload} from 'aws-sdk/lib/s3/managed_upload';
+import { ManagedUpload } from 'aws-sdk/lib/s3/managed_upload';
 import SendData = ManagedUpload.SendData;
-
 
 @Injectable()
 export class S3Service {
-  private s3:  AWS.S3;
-  private bucketName: string
+  private s3: AWS.S3;
+  private bucketName: string;
+
   constructor() {
     this.s3 = new AWS.S3({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
-        secretAccessKey: process.env.AWS_SECRET_KEY,
-        region: 'eu-north-1',
-        signatureVersion: 'v4',
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      region: 'eu-north-1',
+      signatureVersion: 'v4',
     });
 
     this.bucketName = process.env.AWS_S3_BUCKET_NAME;
@@ -30,19 +30,25 @@ export class S3Service {
       const url = await this.s3.getSignedUrlPromise('getObject', params);
       return url;
     } catch (error) {
-      console.log(
-        `Failed to get presigned URL for key ${key}`,
-        error.stack,
-      );
+      console.log(`Failed to get presigned URL for key ${key}`, error.stack);
     }
   }
 
-  async upload(
-  file: Express.Multer.File
-): Promise<SendData> {
+  
+  private randomName(originalName: string): string {
+    const random1 = Math.random().toString(36).substring(2, 8);
+    const random2 = Math.random().toString(36).substring(2, 8);
+    const fileExtension = originalName.split('.').pop(); 
+    return `file_${random1}_${random2}.${fileExtension}`;
+  }
+
+  
+  async upload(file: Express.Multer.File): Promise<SendData> {
+    const randomFileName = this.randomName(file.originalname);
+
     const params = {
       Bucket: this.bucketName,
-      Key: String(file.originalname),
+      Key: randomFileName, 
       Body: file.buffer,
       ContentType: file.mimetype,
       ContentDisposition: 'inline',
@@ -55,13 +61,7 @@ export class S3Service {
       return await this.s3.upload(params).promise();
     } catch (e) {
       console.log('Could not upload file to s3', { e, mimetype: file.mimetype });
-
-      throw e
-
+      throw e;
     }
-
   }
 }
-
-
-
